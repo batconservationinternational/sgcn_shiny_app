4c6b792189c82c6ab0fd35509432fb68ab14d27d
ngoodby <46496548+ngoodby@users.noreply.github.com>
2022-10-03 12:41:58 -0700

    initial commit

---
+++ app.R
@@ -0,0 +1,61 @@
+library(shiny)
+library(tidyverse)
+library(leaflet)
+library(here)
+library(sf)
+library(spData)
+library(htmltools)
+library(shinyWidgets)
+
+data <- read_csv(here::here("data", "sgcn_final_data.csv")) %>% select(-1)
+spp_choices <- names(data[2:(length(data)-1)])
+states <- us_states["NAME"]
+df <- data %>% left_join(states, by = c("states_2015"="NAME")) %>% st_as_sf() %>% 
+  st_transform("WGS84") %>%
+  mutate(map_labels = gsub(",", ",<br>", text))
+
+# Define UI for application
+ui <- fluidPage(
+
+    titlePanel("Bat Species of Greatest Conservation Need"),
+    br(),
+    sidebarLayout(
+      sidebarPanel(
+        pickerInput('spp', "Select species to see which states consider them species SGCN.", 
+                    multiple = TRUE, choices = spp_choices, 
+                    options = list(`actions-box` = TRUE), selected = spp_choices[1])
+      ),
+    
+
+    mainPanel(
+      strong("Hover on a state to see which species it considers SGCN"),
+      br(),
+      p("If multiple species are selected, any state where one or more of those species are 
+        SGCN will be highlighted"),
+      leafletOutput("spp_map")
+      )
+    )
+  )
+
+
+server <- function(input, output) {
+
+    map_data_react <- reactive({
+      df %>% select(states_2015, any_of(input$spp)) %>% 
+        filter_at(input$spp, any_vars(.==1))
+    })
+    
+    output$spp_map <- renderLeaflet({
+      
+      leaflet(data = map_data_react()) %>% addTiles() %>% 
+        setView( lng = -97
+                 , lat = 39
+                 , zoom = 4 ) %>% 
+        addPolygons() %>% 
+        addPolygons(data = df, fillOpacity=0, opacity=0, 
+        label = ~lapply(paste0("SGCN in ", states_2015, ': ', map_labels), HTML))
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ src/SGCN_bats.R
@@ -0,0 +1,37 @@
+library(tidyverse)
+library(writexl)
+library(here)
+
+sp <- read_csv(here::here("data", "bat_species.csv")) %>% select(Genus, Species) %>% unite("bat", 1:2, remove=T, sep = " ")
+
+c_names <- c("sci_name", "common_name", "num_2005_states","states_2005", "num_2015_states","states_2015", "tax_group")
+
+sgcn <- read.delim(here::here("data", "SGCN_data.csv"), header = T) %>%  
+  separate(., 1, into = c_names, sep="\\|") %>% select(1,6) %>% 
+  full_join(sp, by = c("sci_name" = "bat")) %>% filter(sci_name %in% sp$bat) %>% 
+  mutate(states_2015 = na_if(states_2015, "")) 
+
+sgcn_long <- sgcn %>% 
+  separate_rows(., states_2015, sep=",") %>% arrange(sci_name)
+
+state_sp_list <- sgcn_long %>% group_by(states_2015) %>% summarise(text = str_c(sci_name, collapse = ", "))
+
+total_sgcn <- sgcn_long %>% group_by(states_2015) %>% summarise(num_SGCN = n())
+
+total_states <- sgcn_long %>% group_by(sci_name) %>% summarise(num_states= n())
+
+sgcn_long$yesno <- if_else(is.na(sgcn_long$states_2015) | sgcn_long$states_2015=="", 0, 1)
+
+data <- sgcn_long %>% pivot_wider(names_from=sci_name, values_from = yesno, values_fill=0) %>% 
+  drop_na(states_2015) %>% arrange(states_2015)
+
+sheets <- list("state list for each species" = sgcn, 
+               "species list for each state" = state_sp_list,
+               "# SGCN by state" = total_sgcn,
+               "# states by SGCN" = total_states,
+               "data" = data)
+
+shiny_data <- data %>% left_join(state_sp_list, by = c('states_2015'='states_2015'))
+
+write.csv(shiny_data, here::here("data", "sgcn_final_data.csv"))
+write_xlsx(sheets, here::here("data", "SGCN_info.xlsx"))
